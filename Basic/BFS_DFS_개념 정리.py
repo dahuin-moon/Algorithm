62f9db6cf0e7ff6c576763dd72f143b8420e428f
dahuin-moon <101034894+dahuin-moon@users.noreply.github.com>
2022-06-08 21:47:45 +0900

    BFS/DFS 개념 주석 추가

--- Basic/BFS_DFS_개념 정리.py
+++ Basic/BFS_DFS_개념 정리.py
@@ -4,6 +4,8 @@
 # 1. visited로 얼마나 탐색 공간을 잘 제한하고
 # 2. visited를 대체할 수 있는 자료형을 만드는가
 
+# graph는 1~8로 구성된 노드 중 연결된 노드만을 나타낸 것.
+# 0은 비어있음(1~8)
 graph = [
     [],
     [2,3,8],
@@ -35,11 +37,13 @@ def bfs(graph, start, visited):
                 queue.append(i)
                 visited[i] = True
 
+# DFS는 1 2 7 6 8 3 4 5 순으로 탐색
 visited = [False]*len(graph)   
 dfs(graph, 1, visited)
 
 print()
 
+# BFS는 1 2 3 8 7 4 5 6 순으로 탐색
 visited = [False]*len(graph)
 bfs(graph, 1, visited)
 
