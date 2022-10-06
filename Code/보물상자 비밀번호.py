b25380ee947088170f79a24a1fe09d3d9644d44e
Dahuin Moon <dahuinmoon@gmail.com>
2022-10-06 22:17:36 +0900

    SWEA(5658)::Simulation

--- Code/보물상자 비밀번호.py
+++ Code/보물상자 비밀번호.py
@@ -1,4 +1,5 @@
 # SWEA(5658)::Simulation
+
 T = int(input())
 n,k = 0,0
 t_box = []
@@ -11,22 +12,24 @@ def initialize():
     t_box_num = set()
 
 def rotate():
-    # CW 방향으로 한 칸씩 shift
+    # 오른쪽으로 한 칸씩 Shift
     tmp = t_box[n-1]
     for i in range(n-1,0,-1):
         t_box[i] = t_box[i-1]
     t_box[0] = tmp
 
 def extract():
-    # 한 변에 해당하는 배열로부터 str 추출
-    # ''.join(list) 형태를 사용했으면 더 편했을 것 !
+    # ''.join(list) 를 알았으면 더 유용했을 것!
+
+    # 4개의 변 모두
     for i in range(4):
         a = '0x'
+        # 각 변의 문자 합침
         for j in range(n//4):
             a += t_box[i*(n//4)+j]
-        # hex로 변환(hex to int)
+        # 16진수를 10진수로 변환
         t_box_num.add(int(a,16))
-        
+
 for test_case in range(1, T + 1):
     initialize()
     extract()
